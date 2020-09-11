9dfc4b6924bd2dcb9c6ec68e6bd92a3b3f693506
stratisMarkou <stratismar@gmail.com>
2020-09-11 11:41:47 +0300

    removed whole _build, and ignored it

--- _build/jupyter_execute/content/convex-optimisation/intro.py
+++
@@ -1,5 +0,0 @@
-# Convex optimisation
-
-These are notes and implementations I made off the course [Convex Optimization I](https://see.stanford.edu/Course/EE364A), taught by [Stephen Boyd](http://web.stanford.edu/~boyd/) from Stanford. You can find the full course materials at the [official course page](https://see.stanford.edu/Course/EE364A). Video lectures can also be found at [videolectures.net](http://videolectures.net/stanfordee364aw08_convex_optimization1/). The course follows the [Convex Optimisation book](https://web.stanford.edu/~boyd/cvxbook/) by Boyd and Vanderberghe, freely available on the web.
-
-A lot of the contents of these notes are a combination of the contents of the lectures and book, except I've filled in some gaps I would have liked to have been explained to me when I read the course.
